32c89e1aed62bbf70900d759087abbe9e29105c5
Robby Cloetens <robby@jayjays.be>
2024-10-16 10:59:43 +0200

    editting the target

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:6000',
+        target: 'https://react-jayjays.vercel.app',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
