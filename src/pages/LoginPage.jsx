f9d14fc97d6653faada9b3fa995d366044d07c06
Robby Cloetens <robby@jayjays.be>
2024-10-16 16:17:07 +0200

    break added

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -99,6 +99,7 @@ const LoginPage = () => {
                         >
                             Login
                         </button>
+                        <br/>
                         <button
                             className='bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline'
                             type='submit'
