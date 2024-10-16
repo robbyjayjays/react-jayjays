f1502b3873b8ee1c9271580055eda75e7120c13f
Robby Cloetens <robby@jayjays.be>
2024-10-16 15:18:34 +0200

    back to previous settings

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -26,7 +26,7 @@ const JobListing = ( { job } ) => {
                     { job.location }
                 </div>
                 <Link
-                    to={`/get-job/${id}`}
+                    to={`/job/${id}`}
                     className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                 >
                 Read More
