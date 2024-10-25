b1e65e5de84f7756ac40889300ce7249d76f8747
Robby Cloetens <robby@jayjays.be>
2024-10-25 14:33:06 +0200

    forgot password changes

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -31,7 +31,7 @@ function ForgotPasswordPage() {
           <div className='bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0'>
             <form onSubmit={handleSubmit}>
               <h2 className='text-3xl text-center font-semibold mb-6'>
-                Forgot Password
+                Forgot Password?
               </h2>
 
               <div className='mb-4'>
